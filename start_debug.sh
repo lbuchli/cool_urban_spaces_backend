ad2a9101ef126479b777d7b057491ac4f5876272
Lukas Buchli <lukas.buchli@ost.ch>
2021-08-31 13:33:34 +0200

    Initial Commit

---
+++ start_debug.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+poetry run uvicorn cool_urban_spaces_backend.main:app --reload
---
+++ tests/test_cool_urban_spaces_backend.py
@@ -0,0 +1,5 @@
+from cool_urban_spaces_backend import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
